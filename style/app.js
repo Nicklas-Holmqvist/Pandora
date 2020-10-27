cd5e8b29a32f910ff4420e731866d562a2d37ae7
nicklas-holmqvist <nicklas_holmqvist@outlook.com>
2020-10-27 08:40:47 +0100

    Added important functions

--- style/app.js
+++ style/app.js
@@ -29,3 +29,59 @@ function nextPart3() {
     const partFourHeight = document.querySelector('#part-4').style.height = "100%";
     const partFourDisplay = document.querySelector('#part-4').style.display = 'flex';
 }
+
+document.querySelector('#chooseName').addEventListener('click', getMyName);
+
+
+function getMyName() {
+
+    const getName = document.querySelector('#myNameInput').value;
+    console.log(getName)
+
+    if (getName == "Karl" || getName == "karl") {
+        document.querySelector('#myNameIs').innerText = "Du har bra smak!"
+
+    }
+
+    else if (getName == "David" || getName == "david") {
+        document.querySelector('#myNameIs').innerText = "Hehe, inte så troligt! Hej Karl!"
+    }
+
+    else {
+        document.querySelector('#myNameIs').innerText = "Hej " + getName + ", det var ju ett fint namn. Men jag tror jag kallar dig Karl!"
+    }
+}
+
+document.querySelector('#chooseStatus').addEventListener('click', playerMorningStatus);
+
+function playerMorningStatus() {
+
+    const myStatus = document.querySelector('#isTiredInMorning').value;
+
+    if (myStatus == "Ja" || myStatus == "ja") {
+        document.querySelector('#myStatus').innerText = "Nej, nej, nej... det blir inget roligt spel om du väljer detta. Välj det andra!"
+    }
+
+    else if (myStatus == "Nej" || myStatus == "nej") {
+        document.querySelector('#myStatus').innerText = "Intressant, hoppas du inte får kicken!"
+    }
+
+    else {
+        document.querySelector('#myStatus').innerText = "Det är tyvärr inget val!"
+    }
+}
+
+document.querySelector('#lockUp').addEventListener('click', lockUp)
+
+const lockUpResult = document.querySelector('#resultat');
+
+function lockUp() {
+    const getNr1 = document.querySelector('#nr1').value;
+
+    if ( getNr1 == 1224) {
+        lockUpResult.innerText = "Äntligen! Nu får jag skynda mig."
+    }
+    else {
+        lockUpResult.innerText = "Helvete då, vá det inte 1224?";
+    }
+}
