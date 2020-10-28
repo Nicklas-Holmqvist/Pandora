5d9f6c1099bf05e825a85910488c9182e8608232
nicklas-holmqvist <nicklas_holmqvist@outlook.com>
2020-10-28 18:35:54 +0100

    Added som finishing touches

--- style/app.js
+++ style/app.js
@@ -33,7 +33,7 @@ function playerMorningStatus() {
 
     const myStatus = document.querySelector('#isTiredInMorning').value;
 
-    if (myStatus == "Klösmage" || myStatus == "klösmage") {
+    if (myStatus == "Vek" || myStatus == "vek") {
         document.querySelector('#myStatus').innerText = "Oj oj oj..." + myName + " det här kommer inte bli lätt!";
     }
 
@@ -48,9 +48,6 @@ function playerMorningStatus() {
 
 // Välj vapen
 
-// const hideLockup = document.querySelector('#showLockUp');
-// hideLockup.style.display = "none";
-
 document.querySelector('#chooseWeaponScene').addEventListener('click', playerWeapon);
 
 const inputWeapon = document.querySelector('#whatWeapon');
@@ -61,19 +58,17 @@ function playerWeapon() {
     const whatWeapon = document.querySelector('#whatWeapon').value;
 
     if (whatWeapon == "Pistol" || whatWeapon == "pistol") {
-        textWeapon.innerText = "Jag tar nog bussen, de går ju rätt ofta"
+        nextPartToPistolRegret()
     }
-
     else if (whatWeapon == "Shotgun" || whatWeapon == "shotgun") {
-        textWeapon.innerText = "Hoppas jag slipper köerna bara!"
+        nextPartToShotgunRegret()
     }
     else if (whatWeapon == "PSG" || whatWeapon == "psg") {
-        textWeapon.innerText = "Hoppas jag slipper köerna bara!"
+        nextPartToPsgRegret()
     }
     else if (whatWeapon == "Fatman" || whatWeapon == "fatman") {
-        textWeapon.innerText = "Hoppas jag slipper köerna bara!"
+        nextPartToFatmanRegret()
     }    
-
     else {
         resultat.innerText = "Det är tyvärr inget vapen!!!"
     }
@@ -83,16 +78,20 @@ function playerWeapon() {
 
 document.querySelector('#lockUp').addEventListener('click', lockUp)
 
-const lockUpResult = document.querySelector('#resultat');
+const lockUpResult = document.querySelector('#lockIsUp');
 
 function lockUp() {
-    const getNr1 = document.querySelector('#nr1').value;
+    const getSafeNr = document.querySelector('#getNumber').value;
 
-    if ( getNr1 == 1224) {
-        lockUpResult.innerText = "Äntligen! Nu får jag skynda mig."
+    if ( getSafeNr == 7274) {
+        lockUpResult.innerText = "Testa vänd på siffrorna! 7 kanske är 1 eller 2?";
+    }
+
+    else if ( getSafeNr == 1224) {
+        lockUpResult.innerText = "Äntligen! Nu ska vi se vad myName har att leka med!";
     }
     else {
-        lockUpResult.innerText = "Helvete då, vá det inte 1224?";
+        lockUpResult.innerText = "Rösten talar till " + myName + " - testa 1224!";
     }
 }
 
@@ -103,25 +102,25 @@ document.querySelector('#part-4-5').addEventListener('click', nextPart5);
 document.querySelector('#part-5-6').addEventListener('click', nextPart6);
 document.querySelector('#part-6-7').addEventListener('click', nextPart7);
 
-document.querySelector('#part-8-7').addEventListener('click', nextPart8);
-document.querySelector('#part-8-9').addEventListener('click', nextPart9);
-document.querySelector('#part-9-10').addEventListener('click', nextPart10);
-// document.querySelector('#part-10-20').addEventListener('click', nextPart11);
+document.querySelector('#part-8-7').addEventListener('click', nextPartIfShotgunRegretYes);
+document.querySelector('#part-8-9').addEventListener('click', nextPart8);
+document.querySelector('#part-9-10').addEventListener('click', nextPart9);
+document.querySelector('#part-10-20').addEventListener('click', nextPart10);
 
-document.querySelector('#part-11-7').addEventListener('click', nextPart11);
-document.querySelector('#part-11-12').addEventListener('click', nextPart12);
-document.querySelector('#part-12-13').addEventListener('click', nextPart13);
-// document.querySelector('#part-13-20').addEventListener('click', nextPart5);
+document.querySelector('#part-11-7').addEventListener('click', nextPartIfPistolRegretYes);
+document.querySelector('#part-11-12').addEventListener('click', nextPart11);
+document.querySelector('#part-12-13').addEventListener('click', nextPart12);
+document.querySelector('#part-13-20').addEventListener('click', nextPart13);
 
-document.querySelector('#part-14-7').addEventListener('click', nextPart14);
-document.querySelector('#part-14-15').addEventListener('click', nextPart15);
-document.querySelector('#part-15-16').addEventListener('click', nextPart16);
-// document.querySelector('#part-16-20').addEventListener('click', nextPart5);
+document.querySelector('#part-14-7').addEventListener('click', nextPartIfPsgRegretYes);
+document.querySelector('#part-14-15').addEventListener('click', nextPart14);
+document.querySelector('#part-15-16').addEventListener('click', nextPart15);
+document.querySelector('#part-16-20').addEventListener('click', nextPart16);
 
-document.querySelector('#part-17-7').addEventListener('click', nextPart17);
-document.querySelector('#part-17-18').addEventListener('click', nextPart18);
-document.querySelector('#part-18-19').addEventListener('click', nextPart19);
-// document.querySelector('#part-19-20').addEventListener('click', nextPart5);
+document.querySelector('#part-17-7').addEventListener('click', nextPartIfFatmanRegretYes);
+document.querySelector('#part-17-18').addEventListener('click', nextPart17);
+document.querySelector('#part-18-19').addEventListener('click', nextPart18);
+document.querySelector('#part-19-20').addEventListener('click', nextPart19);
 
 
 // Scene changeBtn
@@ -160,7 +159,7 @@ function nextPart7() {
 // Shotgun scenes
 
 function nextPartToShotgunRegret() {
-    const partOneHeight = document.querySelector('#part-7').style.marginLeft = "-100%";
+    const partOneHeight = document.querySelector('#part-7').style.marginRight = "-100%";
     const partTwoHeight = document.querySelector('#part-8').style.marginLeft = "0";
 }
 
@@ -193,7 +192,7 @@ function nextPartToPistolRegret() {
 
 // Pistol change firearm
 // Change weapon
-function nextPartIfShotgunRegretYes() {
+function nextPartIfPistolRegretYes() {
     const partOneHeight = document.querySelector('#part-11').style.marginLeft = "-100%";
     const partTwoHeight = document.querySelector('#part-7').style.marginLeft = "0";
 }
